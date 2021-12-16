00ccac534d30e85005cd964ce9c45a11802cef5f
fbafica <fbafica@student.42sp.org.br>
2021-12-15 22:11:04 -0300

    echo quotes fix

--- source/builtins/echo.c
+++ source/builtins/echo.c
@@ -6,11 +6,44 @@
 /*   By: fbafica <fbafica@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 16:36:05 by fbafica           #+#    #+#             */
-/*   Updated: 2021/12/15 18:17:48 by fbafica          ###   ########.fr       */
+/*   Updated: 2021/12/15 22:10:30 by fbafica          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static	int	who_opend(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == '"')
+			return (1);
+		if (s[i] == "'"[0])
+			return (2);
+	}
+	return (0);
+}
+
+static int	print_quote(char *s, int index)
+{
+	char	*tmp;
+	int		print_quote;
+
+	tmp = ft_substr(s, 0, index);
+	if (!who_opend(tmp))
+		print_quote = 0;
+	else if (s[index] == "'"[0] && who_opend(tmp) == 1)
+		print_quote = 1;
+	else if (s[index] == '"' && who_opend(tmp) == 2)
+		print_quote = 1;
+	else
+		print_quote = 0;
+	free(tmp);
+	return (print_quote);
+}
 
 static void	print_arg(char *arg)
 {
@@ -23,7 +56,11 @@ static void	print_arg(char *arg)
 	{
 		if (i < arg_len)
 		{
-			ft_putchar_fd(arg[i], 1);
+			if ((arg[i] == '"' || arg[i] == "'"[0]) \
+			&& !print_quote(arg, i))
+				ft_putchar_fd('\0', 1);
+			else
+				ft_putchar_fd(arg[i], 1);
 		}
 		++i;
 	}
